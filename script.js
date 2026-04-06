67978d81b93cd25cbe1b8aeea7dd40aa7b0fe37b
leon gabriel <gabrielg.andrade77@gmail.com>
2026-04-06 13:42:59 -0300

    att06/04

--- script.js
+++ script.js
@@ -6,18 +6,59 @@ const classe = document.querySelector("#classe").value;
 const ability = document.querySelector('#ability').value;
 const imagem = document.querySelector('#imagem').value
 
-
-//console.log(name);
-//console.log(classe);
-//console.log(ability);
-//console.log(imagem);
-
 const card = document.querySelector('#card');
 
-card.innerHTML += `<span>${name}</span>`
-card.innerHTML += `<span>${classe}</span>`
-card.innerHTML += `<span>${ability}</span>`
-card.innerHTML += `<img src = "${imagem}"></img>`;
+const novoCard = document.createElement('div');
+    novoCard.classList.add('personagem');
 
- input.value = '';
+    novoCard.innerHTML = `
+        <img src="${imagem}">
+        <div class="info">
+            <span class="nome"><strong>Nome:</strong> ${name}</span>
+            <span class="classe"><strong>Classe:</strong> ${classe}</span>
+            <span class="ability"><strong>Habilidade:</strong> ${ability}</span>
+            <button type="button" class="alterar">Alterar</button>
+            <button type="button" class="remover">Remover</button>
+        </div>
+    `;
+
+const botaoAlterar = novoCard.querySelector('.alterar');
+
+    botaoAlterar.addEventListener('click', () => {
+        const nomeEl = novoCard.querySelector('.nome');
+        const classeEl = novoCard.querySelector('.classe');
+        const abilityEl = novoCard.querySelector('.ability');
+
+        const novoNome = prompt('Novo nome:');
+        const novaClasse = prompt('Nova classe:');
+        const novaAbility = prompt('Nova habilidade:');
+
+        if (novoNome !== null) {
+            nomeEl.innerHTML = `<strong>Nome:</strong> ${novoNome}`;
+        }
+
+        if (novaClasse !== null) {
+            classeEl.innerHTML = `<strong>Classe:</strong> ${novaClasse}`;
+        }
+
+        if (novaAbility !== null) {
+            abilityEl.innerHTML = `<strong>Habilidade:</strong> ${novaAbility}`;
+        }
+    });
+
+    const botaoRemover = novoCard.querySelector('.remover');
+
+    botaoRemover.addEventListener('click', () => {
+        novoCard.remove();
+    });
+
+    card.appendChild(novoCard);
+
+ document.querySelector('#name').value = '';
+    document.querySelector('#classe').value = '';
+    document.querySelector('#ability').value = '';
+    document.querySelector('#imagem').value = '';
+
+
+    
 }
