319e9407fcb98f97e47c0971bc3196de9c938e25
leon gabriel <gabrielg.andrade77@gmail.com>
2026-04-08 16:03:05 -0300

    fix troca de imagem

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function criarPersonagem(event){
+function criarPersonagem(event) {
     event.preventDefault();
 
-const name = document.querySelector('#name').value;
-const classe = document.querySelector("#classe").value;
-const ability = document.querySelector('#ability').value;
-const imagem = document.querySelector('#imagem').value
+    const name = document.querySelector('#name').value;
+    const classe = document.querySelector("#classe").value;
+    const ability = document.querySelector('#ability').value;
+    const imagem = document.querySelector('#imagem').value
 
-const card = document.querySelector('#card');
+    const card = document.querySelector('#card');
 
-const novoCard = document.createElement('div');
+    const novoCard = document.createElement('div');
     novoCard.classList.add('personagem');
 
     novoCard.innerHTML = `
-        <img src="${imagem}">
+        <img class="imagem" src="${imagem}">
         <div class="info">
             <span class="nome"><strong>Nome:</strong> ${name}</span>
             <span class="classe"><strong>Classe:</strong> ${classe}</span>
@@ -22,16 +22,18 @@ const novoCard = document.createElement('div');
         </div>
     `;
 
-const botaoAlterar = novoCard.querySelector('.alterar');
+    const botaoAlterar = novoCard.querySelector('.alterar');
 
     botaoAlterar.addEventListener('click', () => {
         const nomeEl = novoCard.querySelector('.nome');
         const classeEl = novoCard.querySelector('.classe');
         const abilityEl = novoCard.querySelector('.ability');
+        const imageEl = novoCard.querySelector('.imagem')
 
         const novoNome = prompt('Novo nome:');
         const novaClasse = prompt('Nova classe:');
         const novaAbility = prompt('Nova habilidade:');
+        const novaImage = prompt('Nova imagem:');
 
         if (novoNome !== null) {
             nomeEl.innerHTML = `<strong>Nome:</strong> ${novoNome}`;
@@ -44,7 +46,17 @@ const botaoAlterar = novoCard.querySelector('.alterar');
         if (novaAbility !== null) {
             abilityEl.innerHTML = `<strong>Habilidade:</strong> ${novaAbility}`;
         }
+        if(novaImage !== null){
+
+            console.log(imageEl);
+            imageEl.src = novaImage;
+            console.log(imageEl);
+        }
+
+            console.log(novaImage);
+
     });
+    
 
     const botaoRemover = novoCard.querySelector('.remover');
 
@@ -54,14 +66,14 @@ const botaoAlterar = novoCard.querySelector('.alterar');
 
     card.appendChild(novoCard);
 
- document.querySelector('#name').value = '';
+    document.querySelector('#name').value = '';
     document.querySelector('#classe').value = '';
     document.querySelector('#ability').value = '';
     document.querySelector('#imagem').value = '';
 
-if (!name || !classe || !ability || !imagem) {
-    alert("Preencha todos os campos, aventureiro.");
-    return;
-}
-    
+    if (!name || !classe || !ability || !imagem) {
+        alert("Preencha todos os campos, aventureiro.");
+        return;
+    }
+
 }
